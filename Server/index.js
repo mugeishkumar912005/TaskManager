a6bbff03cd3870ba2767b3f55da82c8f10992d59
mugeishkumar912005 <kmugeis2005@gmail.com>
2025-06-25 11:13:40 +0530

    Apply .gitignore properly

--- Server/index.js
+++ Server/index.js
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 mongoose
-  .connect(process.env.MONGO_URI || "mongodb://127.0.0.1:27017/taskmanager")
+  .connect(process.env.MONGO_URL)
   .then(() => console.log("✅ MongoDB connected"))
   .catch((err) => console.error("❌ MongoDB connection error:", err));
 
